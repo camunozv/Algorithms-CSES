8d3ee3922709e5fd069b7bba7d6c184a9e71e1d2
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2023-09-16 22:09:24 -0500

    Repetitions solved

--- 1-IntroductoryProblems/3-Repetitions/src/main.cpp
+++ 1-IntroductoryProblems/3-Repetitions/src/main.cpp
@@ -5,22 +5,54 @@ int main()
 {
 
     string DNAchain;
-    string DNAsubchain;
     cin >> DNAchain;
 
-    // 1. create the candidate to the greatest subchain.
+    // 1. count the number of words
     int i = 0;
-    while (i < DNAchain.length() && DNAchain[i] == DNAchain[i + 1])
+    int wordCounter = 0;
+    while (i < (int)DNAchain.length())
     {
-        DNAsubchain += DNAchain[i];
+        if (DNAchain[i] != DNAchain[i + 1])
+        {
+            wordCounter++;
+        }
         i++;
     }
 
-    int currentSize = i; // current length of the candidate to greatest chain
-    
-    // 2. Find the greatest subchain
-    
-    cout << currentSize << endl;
+    // 2. get the length of the words.
+    int j = 0;
+    int k = 0; // length per word
+    int n = 0; // index of the words length array
+    int wordsLength[wordCounter];
+    while (j < (int)DNAchain.length())
+    {
+        if (DNAchain[j] == DNAchain[j + 1])
+        {
+            k++; // counts length of a word - 1
+        }
+        else
+        {
+            k++;                // we have to count the last character
+            wordsLength[n] = k; // save the length of that word
+            k = 0;              // reset the length counter
+            n++;                // move the index of the length of the words array, for waiting for the next word
+        }
+        j++;
+    }
 
+    // 3. get the length of the greatest subchain
+    int p = 1;
+    int maxLength = wordsLength[0];
+    while (p < wordCounter)
+    {
+        int wordLength = wordsLength[p];
+        if (wordLength > maxLength)
+        {
+            maxLength = wordLength;
+        }
+        p++;
+    }
+
+    cout << maxLength << endl;
     return 0;
 }
