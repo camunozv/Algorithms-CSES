4bd4518fc04cda3a53428766cd295335c46395ea
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2023-09-30 20:12:39 -0500

    finished but bugs pending to fix

--- 1-IntroductoryProblems/8-TwoSets/src/main.cpp
+++ 1-IntroductoryProblems/8-TwoSets/src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-bool checkIfSolution(int k);
+bool checkIfSolution(long unsigned int k);
 int main()
 {
     long unsigned int n = 0;
@@ -24,50 +24,63 @@ int main()
         long unsigned int setSum = ((n) * (n + 1)) / 4;
 
         // create array where we want to save the combination
-        long unsigned int combinationArrayLength = ((n) / 2) + 1;
-        long unsigned int combination[combinationArrayLength];
+        long unsigned int combination[n];
 
         // cycle which finds out pairs, first and last element pairs.
         long unsigned int a = 0;
         long unsigned int b = n - 1;
-        long unsigned int pairSum = array[a] + array[b];
-        long unsigned int i = 0;
+        long unsigned int i = 1;
+        long unsigned int superior = array[b];
+        long unsigned int inferior = array[a];
 
-        do
+        long unsigned int sum = 0;
+
+        while (sum < setSum)
         {
-            setSum -= pairSum;             // subtract the first pair
-            combination[i] = array[a];     // save minor member of the pair
-            combination[i + 1] = array[b]; // save greatest member of the pair
-            array[a] = -1;                 // cross the members of the pair
-            array[b] = -1;
-            a++; // prepare for selecting the next pairs
-            b--;
-            pairSum = array[a] + array[b]; // update pair sum with the new indexes
-            i += 2;                        // prepare the space for the new data in the combination array
-
-        } while (setSum > pairSum);
-
-        if (setSum != 0)
-        {
-            combination[i] = setSum;
-        }
-
-        for (long unsigned int r = 0; r < combinationArrayLength; r++)
-        {
-            cout << combination[r] << " ";
-        }
-
-        cout << " " << endl;
-
-        for(long unsigned int v = 0; v < n; v++)
-        {
-            if (array[v] != -1)
+            if (sum != setSum)
             {
-                cout << array[v] << " ";
+                sum += superior;
+                combination[i - 1] = superior;
             }
+
+            if (sum != setSum)
+            {
+                sum += inferior;
+                combination[i] = inferior;
+            }
+
+            b--;
+            a++;
+            superior = array[b];
+            inferior = array[a];
+            i += 2;
+        }
+
+        long unsigned int j = 0;
+        cout << i - 1 << endl;
+        while (j < i)
+        {
+            cout << combination[j] << " ";
+            j++;
+        }
+
+        long unsigned int l = array[i - 1] - 2;
+        // long unsigned int k = array[i - 2] - 2;
+
+        while (l < n)
+        {
+            combination[l] = array[l];
+            l++;
+        }
+        cout << "" << endl;
+        while (i < n)
+        {
+            cout << combination[i] << " ";
+            i++;
         }
 
         cout << " " << endl;
+        cout << n - i + 1 << endl;
     }
     else
     {
@@ -77,9 +90,8 @@ int main()
     return 0;
 }
 
-bool checkIfSolution(int k)
+bool checkIfSolution(long unsigned int k)
 {
-    int gaussSum = ((k) * (k + 1)) / 2;
-    cout << gaussSum << endl;
+    long unsigned int gaussSum = ((k) * (k + 1)) / 2;
     return gaussSum % 2 == 0;
 }
