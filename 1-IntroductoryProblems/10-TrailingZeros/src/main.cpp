5dee3b7249ffc61851b5abd04f7a916c15a73527
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2023-10-09 11:50:57 -0500

    Trailing zeros solved

--- 1-IntroductoryProblems/10-TrailingZeros/src/main.cpp
+++ 1-IntroductoryProblems/10-TrailingZeros/src/main.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h> // includes several standard libraries, including <iostream>
+#include <bits/stdc++.h>
 #define LLUI unsigned long long int
 using namespace std;
 
@@ -7,17 +7,7 @@ int main()
 {
     LLUI n = 0;
     cin >> n;
-
-    // Multiplicar hasta antesito de irme a overflow. Justo ahi contar el número de ceros. Guardar el resultado.
-    // Reiniciar la variable acumuladora a 1. Y empezar a multiplicar de nuevo donde me quedé. Así hasta el final
-    // del ciclo.
-
-    // Puedo multiplicar los digitos cada 5 veces contar el número de veces que se divide el número por 10.
-
-    // ¿Cómo verificar antes de entrar a overflow?
-
     cout << trailingZeros(n) << "\n";
-    cout << "Well done nigger \n";
     return 0;
 }
 
@@ -29,22 +19,13 @@ int trailingZeros(LLUI nummer)
 
     for (LLUI i = 5; i <= nummer; i += 5)
     {
-        product = (i) * (i - 1);
-        while (product % 10 == 0)
+        product = i;
+        while (product % 5 == 0)
         {
             zerosCounter++;
-            product = product / 10;
+            product = product / 5;
         }
-
-        product = 1;
     }
 
-    // (a + b) mod m == (a mod m + b mod m) mod m
-    // (a - b) mod m == (a mod m - b mod m) mod m
-    // (a * b) mod m == (a mod m * b mod m) mod m
-    
-    // 8 * 9 * 50 == 3600
-
-    // usar modulo 10 como un hijo de perra
     return zerosCounter;
 }
