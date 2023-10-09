e039d0ee699fe14d213ffdc56534b3830ffaebe1
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2023-10-09 12:07:09 -0500

    Trailing zeros improved, examples added

--- 1-IntroductoryProblems/10-TrailingZeros/src/main.cpp
+++ 1-IntroductoryProblems/10-TrailingZeros/src/main.cpp
@@ -23,9 +23,41 @@ int trailingZeros(LLUI nummer)
         while (product % 5 == 0)
         {
             zerosCounter++;
-            product = product / 5;
+            product /= 5;
         }
     }
 
     return zerosCounter;
 }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define ll long long
+// #define mod (int)(1e9+7)
+// int main(){
+// ios_base::sync_with_stdio(false);  
+// cin.tie(NULL);
+ 
+// ll z=0,n;
+// cin>>n;
+// for(ll i=5;i<=n;i*=5) z+=n/i;
+// cout<<(z);
+// return 0;
+// }
+
+// #include <bits/stdc++.h>
+ 
+// using namespace std;
+ 
+// int main()
+// {
+//     int n, result = 0;
+//     int power5 = 5;
+//     cin >> n;
+//     for (int i = 1; i <= 12; i++)
+//     {
+//         result += n / power5;
+//         power5 *= 5;
+//     }
+//     cout << result << "\n";
+// }
