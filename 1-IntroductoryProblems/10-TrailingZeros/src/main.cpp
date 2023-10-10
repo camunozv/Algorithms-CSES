9bb85a79a8e0f7c2d992ae4d6bdb72ce288d3b6b
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2023-10-10 18:22:07 -0500

    Tried to improve trailing zeros

--- 1-IntroductoryProblems/10-TrailingZeros/src/main.cpp
+++ 1-IntroductoryProblems/10-TrailingZeros/src/main.cpp
@@ -1,13 +1,17 @@
 #include <bits/stdc++.h>
 #define LLUI unsigned long long int
+#define LL long long
 using namespace std;
 
 int trailingZeros(LLUI nummer);
+int trailingZerosGod(LLUI nummer);
 int main()
 {
     LLUI n = 0;
     cin >> n;
     cout << trailingZeros(n) << "\n";
+    cout << trailingZerosGod(n) << "\n";
+
     return 0;
 }
 
@@ -30,14 +34,42 @@ int trailingZeros(LLUI nummer)
     return zerosCounter;
 }
 
+int trailingZerosGod(LLUI nummer)
+{
+    // We need a control variable, but is almost done, the trick is to use the function
+    // log_5(5 * x);
+    LL x = 1;
+    LL r = 5;
+    int z = 0;
+    while (x <= nummer)
+    {
+        r = 5 * x;
+        if (x % 5 != 0)
+        {
+            z++;
+        }
+        else
+        {
+            while (r % 5 == 0)
+            {
+                z++;
+                r /= 5;
+            }
+        }
+        x++;
+    }
+
+    return z;
+}
+
 // #include <bits/stdc++.h>
 // using namespace std;
 // #define ll long long
 // #define mod (int)(1e9+7)
 // int main(){
-// ios_base::sync_with_stdio(false);  
+// ios_base::sync_with_stdio(false);
 // cin.tie(NULL);
- 
+
 // ll z=0,n;
 // cin>>n;
 // for(ll i=5;i<=n;i*=5) z+=n/i;
@@ -46,9 +78,9 @@ int trailingZeros(LLUI nummer)
 // }
 
 // #include <bits/stdc++.h>
- 
+
 // using namespace std;
- 
+
 // int main()
 // {
 //     int n, result = 0;
