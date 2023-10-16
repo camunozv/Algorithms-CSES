fccf0f89f67eaf70a7b1b7a2681d6f1558a60608
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2023-10-15 22:11:00 -0500

    Little improvements traditional sort

--- 1-IntroductoryProblems/12-PalindromeReorder/src/main.cpp
+++ 1-IntroductoryProblems/12-PalindromeReorder/src/main.cpp
@@ -18,55 +18,60 @@ int main()
 
 string palindromeReorder(string baseChain, llui n)
 {
-
+    char temporal = ' ';
     if (n % 2 == 0)
     {
-        for(llui i = 0; i < (n/2); i++)
+
+        for (llui i = 0; i < (n / 2); i++)
         {
-            if (baseChain[i] != baseChain[n-1-i])
+            if (baseChain[i] != baseChain[n - 1 - i])
             {
                 llui k = i + 1;
-                while (k < (n-1-i) && baseChain[k] != baseChain[i])
+                while (k < (n - 1 - i) && baseChain[k] != baseChain[i])
                 {
                     k++;
                 }
 
                 if (baseChain[k] == baseChain[i])
                 {
-                    char temporal = baseChain[n-1-i];
-                    baseChain[n-1-i] = baseChain[k];
+                    temporal = baseChain[n - 1 - i];
+                    baseChain[n - 1 - i] = baseChain[k];
                     baseChain[k] = temporal;
-                } else 
+                }
+                else
                 {
                     baseChain = "NO SOLUTION";
                 }
             }
         }
-    }else
+    }
+    else
     {
         bool onlyOneInTheMiddle = false;
-        for(llui i = 0; i < (n/2); i++)
+        for (llui i = 0; i < (n / 2); i++)
         {
-            if (baseChain[i] != baseChain[n-1-i])
+            if (baseChain[i] != baseChain[n - 1 - i])
             {
                 llui k = i + 1;
-                while (k < (n-1-i) && baseChain[k] != baseChain[i])
+                while (k < (n - 1 - i) && baseChain[k] != baseChain[i])
                 {
                     k++;
                 }
 
                 if (baseChain[k] != baseChain[i] && !onlyOneInTheMiddle)
                 {
-                    char temporal = baseChain[(n/2)];
-                    baseChain[(n/2)] = baseChain[k];
+                    temporal = baseChain[(n / 2)];
+                    baseChain[(n / 2)] = baseChain[k];
                     baseChain[k] = temporal;
                     onlyOneInTheMiddle = true;
-                } else if (baseChain[k] == baseChain[i])
+                }
+                else if (baseChain[k] == baseChain[i])
                 {
-                    char temporal = baseChain[n-1-i];
-                    baseChain[n-1-i] = baseChain[k];
+                    temporal = baseChain[n - 1 - i];
+                    baseChain[n - 1 - i] = baseChain[k];
                     baseChain[k] = temporal;
-                } else 
+                }
+                else
                 {
                     baseChain = "NO SOLUTION";
                 }
