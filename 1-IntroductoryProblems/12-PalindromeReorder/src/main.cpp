f768d9526ed4204a0e23b099854d20c2bac579ad
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2023-10-19 19:05:38 -0500

    Time limit problem solved

--- 1-IntroductoryProblems/12-PalindromeReorder/src/main.cpp
+++ 1-IntroductoryProblems/12-PalindromeReorder/src/main.cpp
@@ -1,10 +1,7 @@
 #include <bits/stdc++.h>
-#define llui unsigned long int
 using namespace std;
 
-llui *characterMapping(string chain, llui n);
-bool allEven(llui *array);
-bool atLeastOneOdd(llui *array);
+int *characterMapping(string chain, int n);
 int main()
 {
     ios::sync_with_stdio(0);
@@ -14,116 +11,75 @@ int main()
 
     cin >> characterChain;
 
-    llui characterChainLength = characterChain.length();
-    llui *mapping = characterMapping(characterChain, characterChainLength);
-    bool hasEvenLength = characterChainLength % 2 == 0 ? true : false;
+    int characterChainLength = characterChain.length();
+    int *mapping = characterMapping(characterChain, characterChainLength);
 
     string sideA;
     string sideB;
-    llui k = 0;
-    llui b = 0;
-    if (hasEvenLength && allEven(mapping))
+    int k = 0;
+    int b = 0;
+    int middle = -1;
+    int middleAppearances = 0;
+
+    int i = 0;
+    while (i < 26 && middleAppearances <= 1)
     {
-
-        for (int i = 0; i < 26; i++)
+        k = mapping[i];
+        if (k % 2 != 0)
         {
-            k = *(mapping + i);
-            b = k / 2;
-            while (k > b)
-            {
-                sideA += char(i + 65);
-                k--;
-            }
-
-            k = *(mapping + 25 - i);
-            b = k / 2;
-            while (k > b)
-            {
-                sideB += char(90 - i);
-                k--;
-            }
+            middle = i;
+            middleAppearances++;
         }
-        cout << sideA + sideB << "\n";
+        i++;
     }
-    else if (!hasEvenLength && atLeastOneOdd(mapping))
+
+    if (middleAppearances > 1)
     {
-        int middle = 0;
-        llui quantityOfMiddle = 0;
-        for (int i = 0; i < 26; i++)
-        {
-            k = mapping[i];
-            b = k / 2;
-            if (k % 2 == 0)
-            {
-                while (k > b)
-                {
-                    sideA += char(i + 65);
-                    k--;
-                }
-            }
-            else
-            {
-                middle = i;
-                quantityOfMiddle = k;
-            }
-        }
-        cout << sideA;
-        while (quantityOfMiddle > 0)
-        {
-            cout << char(middle + 65);
-            quantityOfMiddle--;
-        }
-
-        for (int i = sideA.length() - 1; i >= 0; i--)
-        {
-            cout << sideA[i];
-        }
-
-        cout << "\n";
+        cout << "NO SOLUTION";
     }
     else
     {
-        cout << "NO SOLUTION \n";
+        for (int i = 0; i < 26; i++)
+        {
+            k = mapping[i];
+            b = (k / 2) + (k % 2);
+            while (k > b)
+            {
+                cout << char(i + 65);
+                k--;
+            }
+        }
+
+        if (middleAppearances == 1)
+        {
+            cout << char(middle + 65);
+        }
+
+        for (int i = 25; i >= 0; i--)
+        {
+            k = mapping[i];
+            b = (k / 2) + (k % 2);
+            while (k > b)
+            {
+                cout << char(i + 65);
+                k--;
+            }
+        }
     }
 
-    delete [] mapping;
+    cout << "\n";
+
+    delete[] mapping;
     mapping = NULL;
     return 0;
 }
 
-llui *characterMapping(string chain, llui n)
+int *characterMapping(string chain, int n)
 {
-    llui *array = new llui[26];
-    for (llui i = 0; i < n; i++)
+    int *array = new int[26];
+    for (int i = 0; i < n; i++)
     {
         array[int(chain[i]) - 65]++;
     }
     return array;
-}
-
-bool allEven(llui *array)
-{
-    bool allEven = true;
-    int i = 0;
-    while (i < 26 && allEven)
-    {
-        allEven = array[i] % 2 != 0 ? false : true;
-        i++;
-    }
-    return allEven;
-}
-
-bool atLeastOneOdd(llui *array)
-{
-    int counter = 0;
-    int i = 0;
-    while (i < 26 && counter <= 1)
-    {
-        if (array[i] % 2 != 0)
-        {
-            counter++;
-        }
-        i++;
-    }
-    return counter == 1 ? true : false;
 }
