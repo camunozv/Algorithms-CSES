97567413d3b8ebbaf653d032914a855c0f5e5f04
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2024-03-06 17:35:26 -0500

    Apple division first attempt

--- 1-IntroductoryProblems/16-AppleDivision/src/main.cpp
+++ 1-IntroductoryProblems/16-AppleDivision/src/main.cpp
@@ -1,31 +1,55 @@
 #include <bits/stdc++.h>
-#define LLI long long int
+
 using namespace std;
 
 int main()
 {
-    ios ::sync_with_stdio(0);
+    ios::sync_with_stdio(0);
     cin.tie(0);
 
-    LLI apples = 0;
+    int i = 0;
+    cin >> i;
 
-    cin >> apples;
+    vector<int> my_vector(i);
 
-    LLI weights[apples];
+    for (int j = 0; j < i; j++)
+        cin >> my_vector[j];
 
-    for (LLI i = 0; i < apples; i++)
+    int total_weight = 0;
+
+    for (int k = 0; k < i; k++)
+        total_weight += my_vector[k];
+
+    int index = 0;
+    int best_difference = total_weight;
+    int init_value = 0;
+
+    int sum = 0;
+    int difference_2 = 0;
+    for (int r = 0; r < i; r++)
     {
-        cin >> weights[i];
+        for (int n = 0; n < i; n++)
+        {
+
+            init_value = sum;
+            init_value += my_vector[n];
+
+            difference_2 = total_weight - init_value;
+
+            if (difference_2 < best_difference)
+            {
+                best_difference = difference_2;
+                index = n;
+            }
+        }
+        sum += my_vector[index];
+        total_weight -= sum;
+        my_vector[index] = 0;
     }
 
-    multiset<LLI> appleSet1;
+    int main_difference = sum - total_weight;
+    cout << main_difference << "\n";
+    cout << total_weight << " " << sum << "\n";
 
-    for(LLI i = 0; i < apples; i++)
-    {
-        appleSet1.insert(weights[i]);
-    }
-
-    cout << "\n";
-    
     return 0;
 }
