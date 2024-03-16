acfe7332eb30942119febdeccbb404867ac34bef
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2024-03-15 19:16:09 -0500

    Apple division second attempt

--- 1-IntroductoryProblems/16-AppleDivision/src/main.cpp
+++ 1-IntroductoryProblems/16-AppleDivision/src/main.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+void generate_subsets(int k, int n, vector<int> &elements, vector<bool> &included, vector<int> &set_weight);
+
 int main()
 {
     ios::sync_with_stdio(0);
@@ -11,45 +13,57 @@ int main()
     cin >> i;
 
     vector<int> my_vector(i);
+    vector<bool> included(i);
+    vector<int> set_weight;
 
     for (int j = 0; j < i; j++)
         cin >> my_vector[j];
 
-    int total_weight = 0;
+    generate_subsets(0, my_vector.size(), my_vector, included, set_weight);
 
-    for (int k = 0; k < i; k++)
-        total_weight += my_vector[k];
+    int current_set = 0;
+    int amount_sets = set_weight.size();
+    int difference = 0;
+    int best = pow(10, 9);
 
-    int index = 0;
-    int best_difference = total_weight;
-    int init_value = 0;
-
-    int sum = 0;
-    int difference_2 = 0;
-    for (int r = 0; r < i; r++)
+    for(int i = 0; i < amount_sets; i++)
     {
-        for (int n = 0; n < i; n++)
+        current_set = set_weight[i];
+
+        for(int j = 0; j < amount_sets; j++)
         {
+            if (current_set == set_weight[j]) continue;
 
-            init_value = sum;
-            init_value += my_vector[n];
-
-            difference_2 = total_weight - init_value;
-
-            if (difference_2 < best_difference)
-            {
-                best_difference = difference_2;
-                index = n;
-            }
+            difference = abs(current_set - set_weight[j]);
+            best = min(difference, best);
         }
-        sum += my_vector[index];
-        total_weight -= sum;
-        my_vector[index] = 0;
     }
-
-    int main_difference = sum - total_weight;
-    cout << main_difference << "\n";
-    cout << total_weight << " " << sum << "\n";
+    
+    cout << best << "\n";
 
     return 0;
+}
+
+void generate_subsets(int k, int n, vector<int> &elements, vector<bool> &included, vector<int> &set_weight)
+{
+    if (k == n)
+    {
+        int set_sum;
+        for(int i = 0; i < n; i++)
+        {
+            if (included[i] == 1)
+            {
+                set_sum += elements[i];
+            }
+        }
+        set_weight.push_back(set_sum);
+        set_sum = 0;
+
+    } else 
+    {
+        included[k] = 1;
+        generate_subsets(k + 1, n, elements, included, set_weight);
+        included[k] = 0;
+        generate_subsets(k + 1, n, elements, included, set_weight);
+    }
 }
