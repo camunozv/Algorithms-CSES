a30ed4eee1b14b2cb468a0f923dae2c4e4d9bfce
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2024-03-18 12:26:05 -0500

    Distinct Numbers improved

--- 2-Sorting&Searching/20-DistinctNumbers/src/main.cpp
+++ 2-Sorting&Searching/20-DistinctNumbers/src/main.cpp
@@ -6,18 +6,31 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    // Now we are going to use sorting to speed up the algorithm.
+    // I is also important to note, thath eventhough c++ allows us to use
+    // variable size arrays, that doesn't makes part of the c++ standard
+    // that is why we should use vectors for the same purpose
+
     int n = 0;
     cin >> n;
-    int array[n] = {0};
+    vector<int> array(n);
     for (int i = 0; i < n; i++)
     {
         cin >> array[i];
     }
-    set<int> s;
-    for (int i = 0; i < n; i++)
+
+    sort(array.begin(), array.end());
+
+    int count = 1;
+    for (int i = 0; i < (int) array.size() - 1; i++)
     {
-        s.insert(array[i]);
+        if (array[i] != array[i+1])
+        {
+            count++;
+        }
     }
-    cout << s.size() << "\n";
+
+    cout << count << "\n";
+
     return 0;
 }
