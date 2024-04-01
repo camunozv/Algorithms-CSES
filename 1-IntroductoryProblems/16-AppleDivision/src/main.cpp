9f66978bc89c7ff59fc14c8747ec708054eb48b5
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2024-04-01 18:39:46 -0500

    Apple Division nice approach; two tests pending

--- 1-IntroductoryProblems/16-AppleDivision/src/main.cpp
+++ 1-IntroductoryProblems/16-AppleDivision/src/main.cpp
@@ -2,68 +2,31 @@
 
 using namespace std;
 
-void generate_subsets(int k, int n, vector<int> &elements, vector<bool> &included, vector<int> &set_weight);
-
+int generate_subsets(int k, int n, int sum1, int sum2, vector<int> &elements);
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int i = 0;
-    cin >> i;
+    int n = 0;
+    cin >> n;
 
-    vector<int> my_vector(i);
-    vector<bool> included(i);
-    vector<int> set_weight;
+    vector<int> my_vector(n);
 
-    for (int j = 0; j < i; j++)
-        cin >> my_vector[j];
+    for (int i = 0; i < n; i++)
+        cin >> my_vector[i];
 
-    generate_subsets(0, my_vector.size(), my_vector, included, set_weight);
-
-    int current_set = 0;
-    int amount_sets = set_weight.size();
-    int difference = 0;
-    int best = pow(10, 9);
-
-    for(int i = 0; i < amount_sets; i++)
-    {
-        current_set = set_weight[i];
-
-        for(int j = 0; j < amount_sets; j++)
-        {
-            if (current_set == set_weight[j]) continue;
-
-            difference = abs(current_set - set_weight[j]);
-            best = min(difference, best);
-        }
-    }
-    
-    cout << best << "\n";
-
+    cout << generate_subsets(0, n, 0, 0, my_vector) << "\n";
     return 0;
 }
 
-void generate_subsets(int k, int n, vector<int> &elements, vector<bool> &included, vector<int> &set_weight)
+int generate_subsets(int k, int n, int sum1, int sum2, vector<int> &elements)
 {
+    int minimum = 0;
     if (k == n)
     {
-        int set_sum;
-        for(int i = 0; i < n; i++)
-        {
-            if (included[i] == 1)
-            {
-                set_sum += elements[i];
-            }
-        }
-        set_weight.push_back(set_sum);
-        set_sum = 0;
-
-    } else 
-    {
-        included[k] = 1;
-        generate_subsets(k + 1, n, elements, included, set_weight);
-        included[k] = 0;
-        generate_subsets(k + 1, n, elements, included, set_weight);
+        return abs(sum1 - sum2);
     }
+    minimum = min(generate_subsets(k + 1, n, sum1 + elements[k], sum2, elements), generate_subsets(k + 1, n, sum1, sum2 + elements[k], elements));
+    return minimum;
 }
