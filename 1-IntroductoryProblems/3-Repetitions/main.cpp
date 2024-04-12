8525d8cfb65abeb631fa5024f80d1633915fa288
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2024-04-12 00:45:12 -0500

    Automatic formating learned

--- 1-IntroductoryProblems/3-Repetitions/main.cpp
+++ 1-IntroductoryProblems/3-Repetitions/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
+
 using namespace std;
 
-int main()
-{
+int main() {
 
     string DNAchain;
     cin >> DNAchain;
@@ -10,10 +10,8 @@ int main()
     // 1. count the number of words
     int i = 0;
     int wordCounter = 0;
-    while (i < (int)DNAchain.length())
-    {
-        if (DNAchain[i] != DNAchain[i + 1])
-        {
+    while (i < (int) DNAchain.length()) {
+        if (DNAchain[i] != DNAchain[i + 1]) {
             wordCounter++;
         }
         i++;
@@ -24,14 +22,10 @@ int main()
     int k = 0; // length per word
     int n = 0; // index of the words length array
     int wordsLength[wordCounter];
-    while (j < (int)DNAchain.length())
-    {
-        if (DNAchain[j] == DNAchain[j + 1])
-        {
+    while (j < (int) DNAchain.length()) {
+        if (DNAchain[j] == DNAchain[j + 1]) {
             k++; // counts length of a word - 1
-        }
-        else
-        {
+        } else {
             k++;                // we have to count the last character
             wordsLength[n] = k; // save the length of that word
             k = 0;              // reset the length counter
@@ -43,11 +37,9 @@ int main()
     // 3. get the length of the greatest subchain
     int p = 1;
     int maxLength = wordsLength[0];
-    while (p < wordCounter)
-    {
+    while (p < wordCounter) {
         int wordLength = wordsLength[p];
-        if (wordLength > maxLength)
-        {
+        if (wordLength > maxLength) {
             maxLength = wordLength;
         }
         p++;
