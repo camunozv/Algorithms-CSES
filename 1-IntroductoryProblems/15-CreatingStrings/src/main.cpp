42c622114bc6c3d1577156e3585e24218ecd1101
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2024-04-14 16:58:27 -0500

    Schöne Versuchen Creating Strings

--- 1-IntroductoryProblems/15-CreatingStrings/src/main.cpp
+++ 1-IntroductoryProblems/15-CreatingStrings/src/main.cpp
@@ -2,63 +2,54 @@
 
 using namespace std;
 
-void generate_all(string permut, string reverse_permut);
-string reverse_string(string the_string);
+void lexi_perm(int k, int n, string &text, vector<bool> &included, string &the_string, vector<vector<char>> &non_repeat);
 int main()
 {
-
-    // The complexity must be of O(n!) because the max input size is of 8
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    string permut;
-    cin >> permut;
+    string permutation;
+    cin >> permutation;
 
-    sort(permut.begin(), permut.end());
-    string rev = reverse_string(permut);
+    int n = permutation.length();
+    vector<vector<char>> non_repeat(n);
+    string text = permutation;
+    vector<bool> included(n, false);
 
-    generate_all(permut, rev);
+    sort(permutation.begin(), permutation.end());
+
+    lexi_perm(0, n, text, included, permutation, non_repeat);
     return 0;
 }
 
-void generate_all(string permut, string reverse_permut)
+void lexi_perm(int k, int n, string &text, vector<bool> &included, string &the_string, vector<vector<char>> &non_repeat)
 {
-    if (permut == reverse_permut)
+    if (k == n)
     {
-        return;
+        cout << text << "\n";
     }
     else
     {
-        for (int i = permut.length() - 1; i >= 1; i--)
+        for (int i = 0; i < n; i++)
         {
-            if (permut[i] > permut[i - 1])
+            bool able = true;
+            int j = 0;
+            int r = non_repeat[k].size();
+            char character = the_string[i];
+            while (j < r && able)
             {
-                char temp = permut[i - 1];
-                permut[i - 1] = permut[i];
-                permut[i] = temp;
-                sort(permut.begin() + i, permut.end());
-                temp = permut[i];
-                permut[i] = permut[0];
-                cout << permut << "\n";
-                break;
+                able = non_repeat[k][j] != character;
+                j++;
+            }
+
+            if (able && included[i] == false)
+            {
+                non_repeat[k].push_back(character);
+                included[i] = true;
+                text[k] = character;
+                lexi_perm(k + 1, n, text, included, the_string, non_repeat);
+                included[i] = false;
             }
         }
-        generate_all(permut, reverse_permut);
     }
-}
-
-string reverse_string(string the_string)
-{
-    int half = ((the_string.length() - 1) / 2);
-    int rev_i = the_string.length() - 1;
-
-    for (int i = 0; i <= half; i++)
-    {
-        char temp = the_string[rev_i];
-        the_string[rev_i] = the_string[i];
-        the_string[i] = temp;
-        rev_i--;
-    }
-
-    return the_string;
 }
