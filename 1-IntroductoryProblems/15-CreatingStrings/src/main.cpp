09e9904921f82c15c17f37f3cd5570a4e86a6dd2
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2024-04-13 19:47:01 -0500

    Rosen algorithm attempt

--- 1-IntroductoryProblems/15-CreatingStrings/src/main.cpp
+++ 1-IntroductoryProblems/15-CreatingStrings/src/main.cpp
@@ -2,8 +2,8 @@
 
 using namespace std;
 
-void generate_permutations(string the_string, vector<char> &permutation, vector<bool> &included, int k, int n);
-void generate_subsets(vector<int> &base_array, vector<bool> &included, int k, int n);
+void generate_all(string permut, string reverse_permut);
+string reverse_string(string the_string);
 int main()
 {
 
@@ -11,88 +11,54 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    // GENERATE PERMUTATIONS TEST:
-    // string string_to_sort;
-    // cin >> string_to_sort;
+    string permut;
+    cin >> permut;
 
-    // sort(string_to_sort.begin(), string_to_sort.end());
+    sort(permut.begin(), permut.end());
+    string rev = reverse_string(permut);
 
-    // vector<bool> included(string_to_sort.length(), false);
-    // vector<char> permutation(string_to_sort.length());
-
-    // generate_permutations(string_to_sort, permutation, included, 0, string_to_sort.length());
-
-    // GENERATE SUBSETS TEST:
-
-    int n = 0;
-
-    cin >> n;
-
-    vector<int> base_vector(n);
-    vector<bool> included(n, false);
-    for (int i = 0; i < n; i++)
-    {
-        cin >> base_vector[i];
-    }
-
-    generate_subsets(base_vector, included, 0, n);
-
+    generate_all(permut, rev);
     return 0;
 }
 
-void generate_permutations(string the_string, vector<char> &permutation, vector<bool> &included, int k, int n)
+void generate_all(string permut, string reverse_permut)
 {
-
-    if (k == n) // if the position is the size of the string then we are ended
+    if (permut == reverse_permut)
     {
-        for (auto x : permutation)
-        {
-            cout << x << " ";
-        }
-
-        cout << "\n";
+        return;
     }
     else
     {
-        // call the funciton, generate the next permutation
-        for (int i = 0; i < n; i++)
+        for (int i = permut.length() - 1; i >= 1; i--)
         {
-
-            if (included[i] == false)
+            if (permut[i] > permut[i - 1])
             {
-                included[i] = true;
-                permutation[k] = the_string[i];
-                generate_permutations(the_string, permutation, included, k + 1, n);
-                included[i] = false;
+                char temp = permut[i - 1];
+                permut[i - 1] = permut[i];
+                permut[i] = temp;
+                sort(permut.begin() + i, permut.end());
+                temp = permut[i];
+                permut[i] = permut[0];
+                cout << permut << "\n";
+                break;
             }
         }
+        generate_all(permut, reverse_permut);
     }
 }
 
-void generate_subsets(vector<int> &base_array, vector<bool> &included, int k, int n)
+string reverse_string(string the_string)
 {
-    if (k == n)
-    {
-        // print the permutation
-        for (int i = 0; i < n; i++)
-        {
-            if (included[i] == true)
-            {
-                cout << base_array[i] << " ";
-            }
-        }
-        cout << "\n";
-    }
-    else
-    {
-        // include element, then procede to the next one
-        if (included[k] == false)
-        {
-            included[k] = true;
-            generate_subsets(base_array, included, k + 1, n);
-        }
+    int half = ((the_string.length() - 1) / 2);
+    int rev_i = the_string.length() - 1;
 
-        included[k] = false;
-        generate_subsets(base_array, included, k + 1, n);
+    for (int i = 0; i <= half; i++)
+    {
+        char temp = the_string[rev_i];
+        the_string[rev_i] = the_string[i];
+        the_string[i] = temp;
+        rev_i--;
     }
+
+    return the_string;
 }
