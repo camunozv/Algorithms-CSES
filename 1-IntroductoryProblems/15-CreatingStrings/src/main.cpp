63e67ee9602c64c1979b10517dbabd28c418f94d
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2024-04-16 22:55:59 -0500

    Schöne attempt; 2 aufgabe noch zu lösen

--- 1-IntroductoryProblems/15-CreatingStrings/src/main.cpp
+++ 1-IntroductoryProblems/15-CreatingStrings/src/main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-void lexi_perm(int k, int n, string &text, vector<bool> &included, string &the_string, vector<vector<char>> &non_repeat);
+void lexi_perm(int k, int n, string &text, vector<bool> &included, string &the_string, vector<vector<char>> &non_repeat, int &count, vector<string> &save);
 int main()
 {
     ios::sync_with_stdio(0);
@@ -12,42 +12,48 @@ int main()
     cin >> permutation;
 
     int n = permutation.length();
-    vector<vector<char>> non_repeat(n);
+    vector<vector<char>> non_repeat;
+
+    for (int i = 0; i < n; i++)
+    {
+        vector<char> store(1);
+        non_repeat.push_back(store);
+    }
+
     string text = permutation;
     vector<bool> included(n, false);
-
+    vector<string> save;
+    int count = 0;
     sort(permutation.begin(), permutation.end());
+    lexi_perm(0, n, text, included, permutation, non_repeat, count, save);
 
-    lexi_perm(0, n, text, included, permutation, non_repeat);
+    cout << count << "\n";
+    for (auto s : save)
+    {
+        cout << s << "\n";
+    }
     return 0;
 }
 
-void lexi_perm(int k, int n, string &text, vector<bool> &included, string &the_string, vector<vector<char>> &non_repeat)
+void lexi_perm(int k, int n, string &text, vector<bool> &included, string &the_string, vector<vector<char>> &non_repeat, int &count, vector<string> &save)
 {
     if (k == n)
     {
-        cout << text << "\n";
+        count++;
+        save.push_back(text);
     }
     else
     {
         for (int i = 0; i < n; i++)
         {
-            bool able = true;
-            int j = 0;
-            int r = non_repeat[k].size();
             char character = the_string[i];
-            while (j < r && able)
-            {
-                able = non_repeat[k][j] != character;
-                j++;
-            }
-
+            bool able = non_repeat[k][0] != character;
             if (able && included[i] == false)
             {
-                non_repeat[k].push_back(character);
+                non_repeat[k][0] = character;
                 included[i] = true;
                 text[k] = character;
-                lexi_perm(k + 1, n, text, included, the_string, non_repeat);
+                lexi_perm(k + 1, n, text, included, the_string, non_repeat, count, save);
                 included[i] = false;
             }
         }
