ec408a7784937eea6c40142e1ae0f3389807c541
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2024-04-20 15:26:39 -0500

    Chesboards & Queens solved :D

--- 1-IntroductoryProblems/17-Chessboards&Queens/main.cpp
+++ 1-IntroductoryProblems/17-Chessboards&Queens/main.cpp
@@ -2,7 +2,8 @@
 
 using namespace std;
 
-void n_queen_problem(int &n, int row, int &count, vector<bool> &cols, vector<bool> &diag1, vector<bool> &diag2);
+void n_queen_problem(int &n, int row, int &count, vector<bool> &cols, vector<bool> &diag1, vector<bool> &diag2,
+                     vector<vector<char>> &board);
 
 int main() {
 
@@ -25,32 +26,23 @@ int main() {
     vector<bool> diag1((2 * n) - 1, true);
     vector<bool> diag2((2 * n) - 1, true);
 
-    n_queen_problem(n, 0, count, rows, diag1, diag2);
-
+    n_queen_problem(n, 0, count, rows, diag1, diag2, board);
     cout << count << "\n";
-
     return 0;
 }
 
-void n_queen_problem(int &n, int row, int &count, vector<bool> &cols, vector<bool> &diag1, vector<bool> &diag2) {
+void n_queen_problem(int &n, int row, int &count, vector<bool> &cols, vector<bool> &diag1, vector<bool> &diag2,
+                     vector<vector<char>> &board) {
     if (row == n) {
         count++;
     } else {
         for (int col = 0; col < n; col++) {
-            if (cols[col] && diag1[row + col] && diag2[row - col + n - 1]) {
+            if (board[row][col] == '.' && cols[col] && diag1[row + col] &&
+                diag2[row - col + n - 1]) {
                 cols[col] = diag1[row + col] = diag2[row - col + n - 1] = false;
-                n_queen_problem(n, row + 1, count, cols, diag1, diag2);
+                n_queen_problem(n, row + 1, count, cols, diag1, diag2, board);
                 cols[col] = diag1[row + col] = diag2[row - col + n - 1] = true;
             }
         }
     }
 }
-// Customized keyboard shortcuts
-// alt + f12 -> open terminal
-// alt + 4 -> open run window
-// ctrl + shift + f4 -> close window
-// shift + f10 -> run
-// alt + uparrow -> move line up
-// alt + downarrow -> move line down
-// ctrl + shift + up / down -> move statement up / down
-// ctrl + d -> duplicate line*/
