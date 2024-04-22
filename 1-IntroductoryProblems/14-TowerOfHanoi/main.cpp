3fca81202c53c808ff99c45c17ff5a67ebd13bcb
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2024-04-22 17:48:15 -0500

    Tower of Hanoi Solved :D

--- 1-IntroductoryProblems/14-TowerOfHanoi/main.cpp
+++ 1-IntroductoryProblems/14-TowerOfHanoi/main.cpp
@@ -2,10 +2,8 @@
 
 using namespace std;
 
-pair<int, int> tower_of_hanoi(int n, int sp, int sd, vector<pair<int, int>> &answer);
-
+void tower_of_hanoi(int n, int sp, int sd, vector<pair<int, int>> &answer) ;
 int main() {
-
     ios::sync_with_stdio(0);
     cin.tie(0);
 
@@ -14,32 +12,29 @@ int main() {
 
     vector<pair<int, int>> movements;
 
-    pair<int,int> store = tower_of_hanoi(n, 1, 3, movements);
+    tower_of_hanoi(n, 1, 3, movements);
     cout << movements.size() << "\n";
-
     for (auto mov: movements) {
         cout << mov.first << " " << mov.second << "\n";
     }
-
     return 0;
 }
 
-pair<int, int> tower_of_hanoi(int n, int sp, int sd, vector<pair<int, int>> &answer) {
-
-    if (n == 1) {
+void tower_of_hanoi(int n, int sp, int sd, vector<pair<int, int>> &answer) {
+    if (n == 1){
         pair<int, int> last;
         last.first = sp;
         last.second = sd;
-        return last;
+        answer.push_back(last);
     } else {
-        int newSd = 0;
-        for (int i = 1; i < 4; i++) {
-            if (i != sp && i != sd) {
-                newSd = i;
+        int sd_star = 0;
+        for (int i = 1; i < 4; i++){
+            if (i != sp && i != sd){
+                sd_star = i;
             }
         }
-        pair<int, int> mov = tower_of_hanoi(n - 1, sp, newSd, answer);
-        answer.push_back(mov);
-        return mov;
+        tower_of_hanoi(n-1, sp, sd_star, answer);
+        tower_of_hanoi(1, sp, sd, answer);
+        tower_of_hanoi(n-1, sd_star, sd, answer);
     }
 }
