b20906853bdc6460a24c1587419157ba1b56cbad
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2024-05-21 17:34:04 -0500

    Minimizing coins first overview

---
+++ 3-DynamicProgramming/56-MinimizingCoins/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int min_coin(int &x, vector<int> &coins, vector<int> &value, vector<bool> &calculated);
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n = 0;
+    int x = 0;
+
+    cin >> n;
+    cin >> x;
+
+    vector<int> coins(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> coins[i];
+    }
+
+    return 0;
+}
+
+int min_coin(int &x, vector<int> &coins, vector<int> &value, vector<bool> &calculated) {
+
+    return 0;
+}
