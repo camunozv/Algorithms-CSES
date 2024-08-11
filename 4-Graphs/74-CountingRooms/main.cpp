c62b549d0cf54a466c3c2341fc9dcae075cc7062
camunozv <93873199+camunozv@users.noreply.github.com>
2024-08-11 14:57:15 -0500

    74 Counting Rooms solved

--- 4-Graphs/74-CountingRooms/main.cpp
+++ 4-Graphs/74-CountingRooms/main.cpp
@@ -2,73 +2,66 @@
 
 using namespace std;
 
-void dfs(int i, int j, int &room);
-bool check(int i, int j, vector<vector<char>> &board, vector<vector<bool>> &visited);
+void dfs(int i, int j, int &n, int &m, vector<vector<char>> &board, vector<vector<bool>> &visited);
+
 int main() {
+
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int n = 0;
     int m = 0;
+    int n = 0;
 
     cin >> n;
     cin >> m;
 
-    vector<vector<char>> room_board(n);
+    vector<vector<char>> rooms(n);
     vector<vector<bool>> visited(n);
 
     for (int i = 0; i < n; i++) {
-        vector<char> row(m);
-        vector<bool> vis_rows(m);
-
-        room_board[i] = row;
-        visited[i] = vis_rows;
-
+        vector<char> line(m);
+        vector<bool> bool_line(m, false);
         for (int j = 0; j < m; j++) {
-            cin >> room_board[i][j];
-            visited[i][j] = false;
+            cin >> line[j];
+        }
+        visited[i] = bool_line;
+        rooms[i] = line;
+    }
+
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (!visited[i][j] && rooms[i][j] != '#') {
+                dfs(i, j, n, m, rooms, visited);
+                count++;
+            }
         }
     }
 
-    int counter = 0;
-    for (int i = 0; i < n; i++ ) {
-        for (int j = 0; j < m; j++) {
-
-        }
-    }
+    cout << count << "\n";
     return 0;
+
 }
 
-void dfs(int i, int j, int &room, vector<vector<char>> &board, vector<vector<bool>> &visited) {
+void dfs(int i, int j, int &n, int &m, vector<vector<char>> &board, vector<vector<bool>> &visited) {
 
-    if (visited[i][j]) {
+    if (board[i][j] == '#') {
         return;
-    } else {
-        visited[i][j] = true;
-        if (check(i, j + 1, board, visited)) {
-            dfs(i, j + 1, room, board, visited);
-
-        } else if (check(i + 1, j, board, visited)) {
-            dfs(i + 1, j, room, board, visited);
-
-        } else if (check(i, j - 1, board, visited)) {
-            dfs(i, j - 1, room, board, visited);
-
-        } else if (check(i - 1, j, board, visited)) {
-            dfs(i - 1, j, room, board, visited);
-        } else {
-            room++;
-        }
     }
 
-}
+    visited[i][j] = true;
 
-bool check(int i, int j, vector<vector<char>> &board, vector<vector<bool>> &visited) {
-
-    bool result = true;
-    if (board[i][j] == '#' || visited[i][j]) {
-        result = false;
+    if (i + 1 < n && !visited[i + 1][j]) {
+        dfs(i + 1, j, n, m, board, visited);
+    }
+    if (i - 1 >= 0 && !visited[i - 1][j]) {
+        dfs(i - 1, j, n, m, board, visited);
+    }
+    if (j + 1 < m && !visited[i][j + 1]) {
+        dfs(i, j + 1, n, m, board, visited);
+    }
+    if (j - 1 >= 0 && !visited[i][j - 1]) {
+        dfs(i, j - 1, n, m, board, visited);
     }
 
-    return result;
 }
