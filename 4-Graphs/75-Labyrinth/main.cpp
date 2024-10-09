8fa39bdafd0a5ddbb8b9c768734d07fd1599166f
camunozv <93873199+camunozv@users.noreply.github.com>
2024-10-08 20:49:32 -0500

    BFS for labyrint implemented, distance well but not enough

--- 4-Graphs/75-Labyrinth/main.cpp
+++ 4-Graphs/75-Labyrinth/main.cpp
@@ -2,87 +2,99 @@
 
 using namespace std;
 
-void
-solve(int i, int j, int &n, int &m, vector<vector<char>> &board, queue<tuple<int, int, char>> &the_queue, string &path);
+void bfs(int i, int j, vector<vector<char>> &board, vector<vector<bool>> &visited, vector<vector<int>> &distance) {
+    queue<pair<int, int>> q;
+    visited[i][j] = true;
+    distance[i][j] = 0;
+    pair<int, int> p;
+    p.first = i, p.second = j;
+    q.push(p);
+    int n = board.size(), m = board[0].size();
+
+    while (!q.empty()) {
+        pair<int, int> node = q.front();
+        q.pop();
+        if (node.first + 1 < n && !visited[node.first + 1][node.second] && board[node.first + 1][node.second] != '#') {
+            pair<int, int> a = {node.first + 1, node.second};
+            q.push(a);
+            visited[node.first + 1][node.second] = true;
+            distance[node.first + 1][node.second] = distance[node.first][node.second] + 1;
+        }
+
+        if (node.first - 1 >= 0 && !visited[node.first - 1][node.second] && board[node.first - 1][node.second] != '#') {
+            pair<int, int> a = {node.first - 1, node.second};
+            q.push(a);
+            visited[node.first - 1][node.second] = true;
+            distance[node.first - 1][node.second] = distance[node.first][node.second] + 1;
+        }
+
+        if (node.second - 1 >= 0 && !visited[node.first][node.second - 1] &&
+            board[node.first][node.second - 1] != '#') {
+            pair<int, int> a = {node.first, node.second - 1};
+            q.push(a);
+            visited[node.first][node.second - 1] = true;
+            distance[node.first][node.second - 1] = distance[node.first][node.second] + 1;
+        }
+
+        if (node.second + 1 < m && !visited[node.first][node.second + 1] && board[node.first][node.second + 1] != '#') {
+            pair<int, int> a = {node.first, node.second + 1};
+            q.push(a);
+            visited[node.first][node.second + 1] = true;
+            distance[node.first][node.second + 1] = distance[node.first][node.second] + 1;
+        }
+
+    }
+
+}
 
 int main() {
+
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int n, m;
+    int n = 0;
+    int m = 0;
 
     cin >> n;
     cin >> m;
 
-    vector<vector<char>> labyrinth(n);
-
+    vector<vector<char>> board(n);
+    vector<vector<bool>> visited(n);
+    vector<vector<int>> distance(n);
     for (int i = 0; i < n; i++) {
-        vector<char> line(m);
+        vector<char> row(m);
+        vector<bool> bool_row(m, false);
+        vector<int> int_row(m, 0);
+        board[i] = row;
+        visited[i] = bool_row;
+        distance[i] = int_row;
         for (int j = 0; j < m; j++) {
-            cin >> line[j];
+            cin >> board[i][j];
         }
-        labyrinth[i] = line;
     }
 
-    int i1 = 0;
-    int j1 = 0;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            if (labyrinth[i][j] == 'A') {
-                i = i1;
-                j = j1;
+            if (board[i][j] == 'A') {
+                // Throw the bfs
+                bfs(i, j, board, visited, distance);
+                break;
             }
         }
     }
 
-    queue<tuple<int, int, char>> my_queue;
+    int a = 0;
+    int b = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (board[i][j] == 'B') {
+                a = i;
+                b = j;
+                break;
+            }
+        }
+    }
 
-    string path;
-
-    solve(i1, j1, n, m, labyrinth, my_queue, path);
-
+    cout << distance[a][b] << "\n";
     return 0;
-}
-
-void
-solve(int i, int j, int &n, int &m, vector<vector<char>> &board, queue<tuple<int, int, char>> &the_queue,
-      string &path) {
-
-    tuple<int, int, char> pos_A = make_tuple(i, j, 00);
-    the_queue.push(pos_A);
-
-    while (!the_queue.empty() && board[i][j] != 'B') {
-        tuple<int, int, char> front = the_queue.front();
-
-        i = get<0>(front);
-        j = get<1>(front);
-        path += get<2>(front);
-
-        if (i + 1 < n && board[i + 1][j] != '#') { // down
-            tuple<int, int, char> pos = make_tuple(i + 1, j, 'D');
-            the_queue.push(pos);
-        }
-
-        if (i - 1 >= 0 && board[i - 1][j] != '#') { // up
-            tuple<int, int, char> pos = make_tuple(i - 1, j, 'U');
-            the_queue.push(pos);
-        }
-
-        if (j + 1 < m && board[i][j + 1] != '#') { // right
-            tuple<int, int, char> pos = make_tuple(i, j + 1, 'R');
-            the_queue.push(pos);
-        }
-
-        if (j - 1 >= 0 && board[i][j - 1] != '#') { // left
-            tuple<int, int, char> pos = make_tuple(i, j - 1, 'L');
-            the_queue.push(pos);
-        }
-        the_queue.pop();
-    }
-
-    if (the_queue.empty()) {
-        cout << "NO\n";
-    } else {
-        cout << "YES\n";
-    }
 }
