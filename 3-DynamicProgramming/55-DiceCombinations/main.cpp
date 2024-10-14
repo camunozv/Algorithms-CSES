b3f6c6bf902484a82c5f31cbf39d249c637c56da
camunozv <93873199+camunozv@users.noreply.github.com>
2024-10-14 09:20:30 -0500

    55 Dice Combination solved with iterative approach

--- 3-DynamicProgramming/55-DiceCombinations/main.cpp
+++ 3-DynamicProgramming/55-DiceCombinations/main.cpp
@@ -1,9 +1,21 @@
 #include <bits/stdc++.h>
+
 #define li long int
 
 using namespace std;
 
-li solve(li n, vector<bool> &calculated, vector<li> &value);
+li solve(vector<li> &numbers, int n) {
+    numbers[0] = 1;
+    for (int i = 1; i < n; i++) {
+        for (int c = 1; c < 7; c++) {
+            if (i - c >= 0) {
+                numbers[i] += numbers[i - c];
+            }
+        }
+        numbers[i] = numbers[i] % ((li) pow(10, 9) + 7);
+    }
+    return numbers[n - 1];
+}
 
 int main() {
     ios::sync_with_stdio(0);
@@ -12,33 +24,8 @@ int main() {
     int n = 0;
     cin >> n;
 
-    vector<bool> calculated(n + 1, false);
-    vector<li> value(n + 1, 0);
+    vector<li> values(n + 1, 0);
 
-    cout << solve(n, calculated, value) << "\n";
-
+    cout << solve(values, n + 1) << "\n";
     return 0;
-}
-
-li solve(li n, vector<bool> &calculated, vector<li> &value) {
-
-    li k = 0;
-    if (n == 0) {
-        return 1;
-    } else if (n < 0) {
-        return 0;
-    } else if (calculated[n]) {
-        return value[n];
-    } else {
-        for (int i = 1; i < 7; i++) {
-            k += solve(n - i, calculated, value);
-        }
-
-        calculated[n] = true;
-        k = k % (((li) pow(10, 9)) + 7);
-        value[n] = k;
-
-        return k;
-    }
-
 }
