5331e79d100caa35312e1f295c214bc4a2aef3cd
camunozv <93873199+camunozv@users.noreply.github.com>
2024-10-14 15:45:28 -0500

    56 Minimizing Coins solved with iterative approach

--- 3-DynamicProgramming/56-MinimizingCoins/main.cpp
+++ 3-DynamicProgramming/56-MinimizingCoins/main.cpp
@@ -1,20 +1,30 @@
 #include<bits/stdc++.h>
+#define li long int
 
 using namespace std;
 
-int solve(int x, vector<int> &coins, vector<bool> &value, vector<int> &calculated);
-
-int my_min(int a, int b);
+li solve(int x, vector<int> &coins) {
+    vector<li> values(x + 1, 0);
+    for (int i = 1; i <= x; i++) {
+        values[i] = INT_MAX;
+        for (auto c: coins) {
+            if (i - c >= 0) {
+                values[i] = min(values[i], values[i - c] + 1);
+            }
+        }
+        if (values[i] == INT_MAX) {
+            values[x] = -1;
+        }
+    }
+    return values[x];
+}
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int n = 0;
-    int x = 0;
-
-    cin >> n;
-    cin >> x;
+    int n = 0, x = 0;
+    cin >> n >> x;
 
     vector<int> coins(n);
 
@@ -22,45 +32,6 @@ int main() {
         cin >> coins[i];
     }
 
-    vector<int> possible_values(x + 1, 0);
-    vector<bool> calculated_values(x + 1, false);
-
-    cout << "\n";
-
-    int solution = solve(x, coins, calculated_values, possible_values);
-
-    if (solution == INT_MAX) {
-        cout << -1 << "\n";
-    } else {
-        cout << solution << "\n";
-    }
+    cout << solve(x, coins) << "\n";
     return 0;
-}
-
-int solve(int x, vector<int> &coins, vector<bool> &value, vector<int> &calculated) {
-    if (x == 0) {
-        return 0;
-    } else if (x < 0) {
-        return INT_MIN;
-    } else if (value[x]) {
-        return calculated[x];
-    } else {
-        int best = INT_MAX;
-        for (auto c: coins) {
-            best = my_min(best, solve(x - c, coins, value, calculated) + 1);
-        }
-        value[x] = true;
-        calculated[x] = best;
-        return best;
-    }
-}
-
-int my_min(int a, int b) {
-    int return_value = 0;
-    if (a < b || b < 0) {
-        return_value = a;
-    } else {
-        return_value = b;
-    }
-    return return_value;
 }
