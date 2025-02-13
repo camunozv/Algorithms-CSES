a89f1f2944cf62b2cdb555d7c665e1bd55ca384e
camunozv <93873199+camunozv@users.noreply.github.com>
2025-02-13 16:08:45 -0500

    57 Coin combinations 1: solved with iterative approach

--- 3-DynamicProgramming/57-CoinCombinations1/src/main.cpp
+++ 3-DynamicProgramming/57-CoinCombinations1/src/main.cpp
@@ -1,58 +1,46 @@
 #include <bits/stdc++.h>
 
-#define li long int
-
 using namespace std;
 
-li solve(vector<li> &coins, li x, li &n, vector<bool> &calculated, vector<li> &value);
+int solve(vector<int> &coins, int n)
+{
 
-int main() {
+    vector<int> sol(n + 1, 0);
+    sol[0] = 1;
 
-    ios::sync_with_stdio(0);
+    for (int i = 1; i < n + 1; i++)
+    {
+        for (auto d : coins)
+        {
+            if (i - d >= 0)
+            {
+                sol[i] += sol[i - d];
+            }
+            sol[i] = sol[i] % (int)(pow(10, 9) + 7);
+        }
+    }
+
+    return sol[n];
+}
+
+int main()
+{
+
     cin.tie(0);
+    ios::sync_with_stdio(0);
 
-    li n = 0; // number of coins
-    li x = 0; // target sum
+    int n, x;
 
-    cin >> n;
-    cin >> x;
+    cin >> n >> x;
 
-    vector<li> coins(n);
-    vector<bool> calculated(x + 1, false);
-    vector<li> value(x + 1);
+    vector<int> coins(n);
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cin >> coins[i];
     }
 
-    cout << solve(coins, x, n, calculated, value) << "\n";
+    cout << solve(coins, x) << "\n";
 
     return 0;
-}
-
-li solve(vector<li> &coins, li x, li &n, vector<bool> &calculated, vector<li> &value) {
-
-    li k = 0;
-
-    if (x == 0) {
-        k = 1;
-    } else if (x < 0) {
-        k = -1;
-    } else if (calculated[x]) {
-        k = value[x];
-    } else {
-        li test = 0;
-        for (int i = 0; i < n; i++) {
-            test = solve(coins, x - coins[i], n, calculated, value);
-            if (test > 0) {
-                k += test;
-            }
-        }
-
-        calculated[x] = true;
-        k = (k % (li) (pow(10, 9) + 7));
-        value[x] = k;
-    }
-
-    return k;
 }
