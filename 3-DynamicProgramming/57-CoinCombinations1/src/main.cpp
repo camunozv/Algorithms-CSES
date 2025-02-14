6be17f49090cd35159b02e62f8f99cac53390ad0
camunozv <93873199+camunozv@users.noreply.github.com>
2025-02-13 20:43:03 -0500

    57 coin combination; solution improved

--- 3-DynamicProgramming/57-CoinCombinations1/src/main.cpp
+++ 3-DynamicProgramming/57-CoinCombinations1/src/main.cpp
@@ -7,7 +7,7 @@ int solve(vector<int> &coins, int n)
 
     vector<int> sol(n + 1, 0);
     sol[0] = 1;
-
+    int m = (int)(pow(10, 9) + 7);
     for (int i = 1; i < n + 1; i++)
     {
         for (auto d : coins)
@@ -15,8 +15,8 @@ int solve(vector<int> &coins, int n)
             if (i - d >= 0)
             {
                 sol[i] += sol[i - d];
+                sol[i] = sol[i] % m;
             }
-            sol[i] = sol[i] % (int)(pow(10, 9) + 7);
         }
     }
 
